78eda015e9a840f13ea5454517e9a444fb480058
Tim Caswell <tim@creationix.com>
2010-09-15 12:09:19 -0700

    Make it a little prettier.

--- main.js
+++ main.js
@@ -16,18 +16,30 @@ window.onload = function () {
 
 
   var i = 0;
+  ctx.moveTo(x, y);
   setInterval(function () {
-      i += 0.1;
+      i += 0.01;
+      var a = Math.sin(i);
+      var b = Math.cos(i);
       ctx.beginPath();
-      ctx.moveTo(x, y);
-      x += Math.random() * 20 - 10;
-      y += Math.random() * 20 - 10;
-      if (x < 0) x = 0;
-      else if (x > WIDTH) x = WIDTH;
-      if (y < 0) y = 0;
-      else if (y > HEIGHT) y = HEIGHT;
-      ctx.strokeStyle = 'rgb(255, ' + Math.floor(Math.sin(i) * 128 + 128) + ', ' + Math.floor(Math.cos(i) * 128 + 128) + ')';
-      ctx.lineTo(x, y);
+      ctx.strokeStyle = 'rgba(255, ' + Math.floor(a * 128 + 128) + ', ' + Math.floor(b * 128 + 128) + ', 0.2)';
+      x += a;
+      y += b;
+      for (var z = 0; z < 20; z++) {
+          x += Math.random() * 10 - 5;
+          y += Math.random() * 10 - 5;
+          if (x < 0) x = 0;
+          else if (x > WIDTH) x = WIDTH;
+          if (y < 0) y = 0;
+          else if (y > HEIGHT) y = HEIGHT;
+          ctx.lineTo(x, y);
+          ctx.moveTo(x + 4, y);
+          ctx.lineTo(x, y + 4);
+          ctx.lineTo(x - 4, y);
+          ctx.lineTo(x, y - 4);
+          ctx.lineTo(x + 4, y);
+          ctx.moveTo(x, y);
+      }
       ctx.stroke();
   }, 10);
 }
